8f54a24f71a44f6b02122f2a9a577afb3c8d5765
Mhizdhee <olaniyandorcas91@gmail.com>
2023-09-28 08:23:00 +0100

    new update

--- src/Components/Common/Deposit.jsx
+++ src/Components/Common/Deposit.jsx
@@ -1,36 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../../Styles/Deposit.css";
 import CustomButton from './CustomButton'
 
-const Deposit =()=> {
+const Deposit =({closeModal})=> {
+
+    const [inputValue, setInputValue] = useState("");
+
     const buttonStyle = {
         borderRadius: "23px",
-        background: "#939393",
         color: "#FFFFFF",
         fontFamily: "Inter,sans-serif",
         fontSize: "16px",
         fontWeight: "500",
         padding: "0",
         width:"222px",
+        backgroundColor: inputValue ? " #1D1DB9" : "#939393", 
+
       };
+
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+      };
+    
+
     
   return (
     <>
-        {/* <div className='dep-dep'> */}
-        <div className='dep-container'>
+        <div className='deposit-modal'>
+        <div className='deposit-modal-container'>
             <h3>Deposit</h3>
             <hr />
             <p>Dear user, Please note that all transactions are conducted with our payment partners using a valid debit card</p>
             <input type="text"
-            placeholder='Input amount' />
+            placeholder='Input amount'
+            value={inputValue}
+             onChange={handleInputChange}
+            />
 
             <CustomButton
              buttonText={"Continue"}
-             style={buttonStyle}/>
-            {/* </div> */}
+             style={buttonStyle}
+             onClick={closeModal} 
+             />
+            </div>
         </div>
     </>
   )
 }
 
-export default Deposit
+export default Deposit;
