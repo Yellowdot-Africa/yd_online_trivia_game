13ad23281716704099dcce056d37dbddc6a76c88
Mhizdhee <olaniyandorcas91@gmail.com>
2023-10-08 23:30:36 +0100

    new update

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,25 +1,25 @@
-// import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
-// const AuthContext = createContext();
+const AuthContext = createContext();
 
-// export function AuthProvider({ children }) {
-//   const [user, setUser] = useState(null);
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
 
-//   const login = (username, role) => {
-//     setUser({ username, role });
-//   };
+  const login = (username, role) => {
+    setUser({ username, role });
+  };
 
-//   const logout = () => {
-//     setUser(null);
-//   };
+  const logout = () => {
+    setUser(null);
+  };
 
-//   return (
-//     <AuthContext.Provider value={{ user, login, logout }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// }
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
 
-// export function useAuth() {
-//   return useContext(AuthContext);
-// }
+export function useAuth() {
+  return useContext(AuthContext);
+}
